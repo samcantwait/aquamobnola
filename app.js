516928e616ec0727a6970f86962b02c2887e31cd
Samantha Atkins <samatkins@gmail.com>
2023-03-03 15:58:39 -0600

    add feature for automatically sending form data from nodemailer.

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mail = require('./app/mail');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/response", (req, res) => {
-    const address = req.body.address;
-    console.log("Email: " + address);
-    res.sendFile(__dirname + "/index.html");
+app.post("/subscribe", async (req, res) => {
+    await mail.subscribe(req.body.email).catch(e => { console.log(e) });
+    res.send('Email sent');
+    // res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+app.post("/contact", async (req, res) => {
+    await mail.info(req.body).catch(e => { console.log(e) });
+    res.send(`Message from "${req.body.fullName}". Email address is "${req.body.email}".  The message is: "${req.body.message}"`);
 })
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
 
