dded7e2c0c0b98f91747f74c657c7a0dba0c3e48
Samantha Atkins <samatkins@gmail.com>
2023-03-14 08:31:32 -0500

    combine parallax into observer file. Set observer to trigger parallax only when in view

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 import { bloodFeature } from './blood.js';
 import { tickets_modal } from './tickets_modal.js';
-import { parallax } from './parallax.js';
 import { observer } from './observer.js';
 import { navigationMenu } from './navigation.js';
-// import { slider } from './slider.js';
 
 bloodFeature();
 tickets_modal();
-parallax();
 observer();
 navigationMenu();
 
--- js/observer.js
+++ js/observer.js
@@ -1,9 +1,8 @@
 export const observer = () => {
+
+    //Add observer to the show section so that the photos appear from below as the user scrolls into the section.
     const sectionShow = document.querySelector('.section-show');
     const photos = document.querySelectorAll('.show__photos');
-
-    const sectionMobsters = document.querySelector('.section-mobsters');
-    const castMembers = document.querySelectorAll('.cast');
 
     const moveUp = (entries, showObserver) => {
         entries.forEach(entry => {
@@ -32,10 +31,15 @@ export const observer = () => {
 
     const showObserver = new IntersectionObserver(moveUp, {
         root: null,
-        threshold: .1,
+        threshold: .2,
     });
 
     showObserver.observe(sectionShow);
+
+
+    //Add an observer to the mobsters section so that the cast bios appear from the side as the user scrolls into the section.
+    const sectionMobsters = document.querySelector('.section-mobsters');
+    const castMembers = document.querySelectorAll('.cast');
 
     const grow = (entries, castObserver) => {
         entries.forEach(entry => {
@@ -53,6 +57,7 @@ export const observer = () => {
 
     castObserver.observe(sectionMobsters);
 
+    //Function to check the screen width so a more user friendly design is used.
     const checkWidth = () => {
         if (window.innerWidth <= 512) {
             castMembers.forEach(member => {
@@ -66,6 +71,8 @@ export const observer = () => {
     }
     window.addEventListener('resize', checkWidth)
 
+
+    //Add an observer for every section so that the correct link in the nav list will be highlighted.
     const links = document.querySelectorAll('.navigation__item');
 
     const options = {
@@ -79,7 +86,6 @@ export const observer = () => {
     const active = (entries, sectionObserver) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                console.log(entry)
                 const active = entry.target.id;
                 links.forEach(link => {
                     if (link.dataset.id === active) {
@@ -93,13 +99,49 @@ export const observer = () => {
     }
 
     const sectionObserver = new IntersectionObserver(active, options);
-
     const sections = document.querySelectorAll('section');
-
     const top = document.getElementById('top');
 
     sectionObserver.observe(top);
     sections.forEach(section => {
         sectionObserver.observe(section);
     })
+
+
+    //Function to control the parallax effect on the header section.
+    const handleScroll = () => {
+        const headingMain = document.querySelector('.heading-primary--main');
+        const headingSub = document.querySelector('.heading-primary--sub');
+        // const nav = document.querySelector('.navigation');
+        const headingLower = document.querySelector('.header__title-lower');
+        let scrolled = window.pageYOffset;
+        let rate = scrolled * 0.5;
+
+        headingMain.style.transform = `translate3d(0, ${rate}px, 0)`;
+        // nav.style.transform = `translate3d(0, ${scrolled * 0.75}px, 0)`;
+        headingSub.style.transform = `translate3d(0, ${scrolled * 0.3}px, 0)`;
+        headingLower.style.transform = `translate3d(-50%, ${scrolled * 0.5}px, 0)`;
+    }
+
+    //Add observer to add and remove the parallax effect based on page location
+    const header = document.querySelector('.header');
+
+    const parallax = (entries, headerObserver) => {
+        entries.forEach(entry => {
+            console.log(entry)
+            if (entry.isIntersecting) {
+                window.addEventListener('scroll', handleScroll);
+            } else {
+                window.removeEventListener('scroll', handleScroll);
+            }
+        })
+    }
+
+    const headerObserver = new IntersectionObserver(parallax, {
+        root: null,
+        rootMargin: '0px 0px',
+        threshold: [0, 1],
+    })
+
+    headerObserver.observe(header);
 }
